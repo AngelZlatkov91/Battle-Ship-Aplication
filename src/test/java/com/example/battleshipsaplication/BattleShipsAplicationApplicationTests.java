78f3a4e51bff93f47acbab4b9caac5a576d8222f
AngelZlatkov91 <128607549+AngelZlatkov91@users.noreply.github.com>
2024-03-07 16:44:47 +0200

    battleShip first made database

---
+++ src/test/java/com/example/battleshipsaplication/BattleShipsAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battleshipsaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleShipsAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
